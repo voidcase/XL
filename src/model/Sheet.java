7feef1e66040942488116d4ea588c27bd87dce08
voidcase <dat13ili@student.lu.se>
2014-11-10 15:30:01 +0100

    added shit,slut and slut factory.

---
+++ src/model/Sheet.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Observable;
+
+public class Sheet extends Observable{
+	//TODO
+	public void update(){
+		//TODO
+	}
+}
---
+++ src/model/Slot.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.Observable;
+
+public abstract class Slot extends Observable{
+	//TODO
+	public void update(){
+		//TODO
+	}
+}
