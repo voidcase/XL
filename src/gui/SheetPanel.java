d3f83c4bc6b6d503dec634c848fff0f82b97cf50
erik <dat13egr@student.lth.se>
2014-11-12 17:09:31 +0100

    Added support for observers

--- src/gui/SheetPanel.java
+++ src/gui/SheetPanel.java
@@ -4,8 +4,15 @@ import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.WEST;
 
 public class SheetPanel extends BorderPanel {
+    private SlotLabels slots;
+    
     public SheetPanel(int rows, int columns) {
         add(WEST, new RowLabels(rows));
-        add(CENTER, new SlotLabels(rows, columns));
+        slots = new SlotLabels(rows, columns)
+        add(CENTER, slots);
+    }
+
+    public List<Observer> listObservers() {
+        return slots.listObservers();
     }
 }
--- src/gui/StatusPanel.java
+++ src/gui/StatusPanel.java
@@ -2,10 +2,12 @@ package gui;
 
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.WEST;
+import java.util.ArrayList;
 
 public class StatusPanel extends BorderPanel {
-    protected StatusPanel(StatusLabel statusLabel) {
-        add(WEST, new CurrentLabel());
+
+    protected StatusPanel(StatusLabel statusLabel, CurrentLabel currentLabel) {
+        add(WEST, currentLabel);
         add(CENTER, statusLabel);
     }
 }
