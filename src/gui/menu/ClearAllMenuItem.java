f4fc3242a0497eb5b09180030bbbfaa5578fb66a
voidcase <dat13ili@student.lu.se>
2014-11-17 16:21:28 +0100

    implementerade Clear- och ClearAllMenuItem.

--- src/gui/menu/ClearAllMenuItem.java
+++ src/gui/menu/ClearAllMenuItem.java
@@ -3,14 +3,19 @@ package gui.menu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JMenuItem;
+import model.Sheet;
 
 class ClearAllMenuItem extends JMenuItem implements ActionListener {
-    public ClearAllMenuItem() {
+	
+	Sheet shit;
+	
+    public ClearAllMenuItem(Sheet s) {
         super("Clear all");
         addActionListener(this);
+        shit = s;
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+    	//shit.removeAll();			--TODO väntar på att Love ska implementera i sheet. 
     }
 }
