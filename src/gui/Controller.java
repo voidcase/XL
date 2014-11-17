e286921d184e36e9b68a7c5e1eac9576c1b8e47e
erik <dat13egr@student.lth.se>
2014-11-17 09:59:30 +0100

    Updated controller

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,10 +1,34 @@
 import java.util.Observable;
 
 public class Controller extends Observable {
+    private Sheet sheet;
+    private String current;
 
-    public Controller() {
-
+    public Controller(Sheet sheet) {
+	this.sheet = sheet;
+	current = "A1";
     }
     
-    
+    /**
+     * Sets current slot to address
+     */
+    public void setCurrent(String address) {
+	current = address;
+	notifyObservers();
+    }
+
+    /**
+     * Returns current slot
+     */
+    public String getCurrent() {
+	return current;
+    }
+
+    /**
+     * Enter content into current slot
+     */
+    public void update(String content) {
+	sheet.update(current, content);
+	notifyObservers();
+    }
 }
