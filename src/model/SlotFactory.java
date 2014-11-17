8c4eeef19695f267a6360e211201fd990bd3ff6d
tingiie <fatima.a.alpha@gmail.com>
2014-11-17 20:12:35 +0100

    SlotFactory

--- src/model/SlotFactory.java
+++ src/model/SlotFactory.java
@@ -1,12 +1,36 @@
 package model;
 
+import java.io.IOException;
+
+import expr.Expr;
+import expr.ExprParser;
+
 public class SlotFactory {
+	ExprParser parser;
 	
 	public SlotFactory() {
-		
+		parser = new ExprParser();
 	}
 	
 	public Slot buildSlot(String string) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(string);
+		
+		if(sb.charAt(0) == '#'){
+			sb.deleteCharAt(0);
+			return new CommentSlot(sb.toString());
+		}
+		
+	
+			try {
+				Expr expr = parser.build(string);
+				return new ExprSlot(expr);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
+		
 		return null;
 	}
 }
