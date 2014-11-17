41f84a163b5533bc1b9aee2810abd21fc210b255
tingiie <fatima.a.alpha@gmail.com>
2014-11-17 21:02:50 +0100

    Testpaket + factory

--- src/model/SlotFactory.java
+++ src/model/SlotFactory.java
@@ -7,30 +7,26 @@ import expr.ExprParser;
 
 public class SlotFactory {
 	ExprParser parser;
-	
+
 	public SlotFactory() {
 		parser = new ExprParser();
 	}
-	
+
 	public Slot buildSlot(String string) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(string);
-		
-		if(sb.charAt(0) == '#'){
+
+		if (sb.charAt(0) == '#') {
 			sb.deleteCharAt(0);
 			return new CommentSlot(sb.toString());
 		}
-		
-	
-			try {
-				Expr expr = parser.build(string);
-				return new ExprSlot(expr);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	
-		
-		return null;
+
+		try {
+			Expr expr = parser.build(string);
+			return new ExprSlot(expr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new XLException(e.getMessage());
+		}
 	}
 }
