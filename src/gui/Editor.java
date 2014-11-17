eaa49428ac49f27b210183e63b2ff4ce19dd7cf0
voidcase <dat13ili@student.lu.se>
2014-11-17 13:15:03 +0100

    Implemented Editor.update

--- src/gui/Editor.java
+++ src/gui/Editor.java
@@ -7,13 +7,18 @@ import java.util.Observer;
 import javax.swing.JTextField;
 
 public class Editor extends JTextField implements Observer{
-    public Editor() {
+    
+	Controller ctrl;
+	
+	public Editor(Controller c) {
         setBackground(Color.WHITE);
+        ctrl = c;
+        ctrl.addObserver(this);
     }
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		
+		setText(ctrl.currentString());
 	}
 }
