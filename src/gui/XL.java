0d274c5172427a1d38943f683ddd7252bc4c1135
tingiie <fatima.a.alpha@gmail.com>
2014-11-18 09:44:59 +0100

    kommentar i xl

--- src/gui/XL.java
+++ src/gui/XL.java
@@ -16,58 +16,65 @@ import javax.swing.JPanel;
 import model.Sheet;
 
 public class XL extends JFrame implements Printable {
-    private static final int ROWS = 10, COLUMNS = 8;
-    private XLCounter counter;
-    private StatusLabel statusLabel = new StatusLabel();
-    private CurrentLabel currentLabel = new CurrentLabel();
-    private XLList xlList;
-    private Sheet sheet;
-    private Controller controller;
+	private static final int ROWS = 10, COLUMNS = 8;
+	private XLCounter counter;
+	private StatusLabel statusLabel = new StatusLabel();
+	private CurrentLabel currentLabel = new CurrentLabel();
+	private XLList xlList;
+	private Sheet sheet;
+	private Controller controller;
 
-	//TODO: hold a sheet!
+	// TODO: hold a sheet!
 
-    public XL(XL oldXL) {
-        this(oldXL.xlList, oldXL.counter);
-    }
+	public XL(XL oldXL) {
+		this(oldXL.xlList, oldXL.counter);
+	}
 
-    public XL(XLList xlList, XLCounter counter) {
-        super("Untitled-" + counter);
-        this.xlList = xlList;
-        this.counter = counter;
-        xlList.add(this);
-        counter.increment();
-        JPanel statusPanel = new StatusPanel(statusLabel, currentLabel);
-        JPanel sheetPanel = new SheetPanel(ROWS, COLUMNS);
-        sheet = new Sheet();
-        controller = new Controller(sheet);
-        Editor editor = new Editor(controller);	
-        
-	//TODO: add observers to sheet
-        add(NORTH, statusPanel);
-        add(CENTER, editor);
-        add(SOUTH, sheetPanel);
-        setJMenuBar(new XLMenuBar(this, xlList, statusLabel, controller, sheet));
-        pack();
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
-        setVisible(true);
-    }
+	public XL(XLList xlList, XLCounter counter) {
+		super("Untitled-" + counter);
+		this.xlList = xlList;
+		this.counter = counter;
+		xlList.add(this);
+		counter.increment();
+		JPanel statusPanel = new StatusPanel(statusLabel, currentLabel);
+		JPanel sheetPanel = new SheetPanel(ROWS, COLUMNS);
+		sheet = new Sheet();
+		controller = new Controller(sheet);
+		Editor editor = new Editor(controller);
 
-    public int print(Graphics g, PageFormat pageFormat, int page) {
-        if (page > 0)
-            return NO_SUCH_PAGE;
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-        printAll(g2d);
-        return PAGE_EXISTS;
-    }
+		// TODO: add observers to sheet
+		add(NORTH, statusPanel);
+		add(CENTER, editor);
+		add(SOUTH, sheetPanel);
+		setJMenuBar(new XLMenuBar(this, xlList, statusLabel, controller, sheet)); // kolla
+																					// konstruktorn,
+																					// får
+																					// sheet
+																					// och
+																					// controller
+																					// skickas
+																					// in?
+		pack();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
+	}
 
-    public void rename(String title) {
-        setTitle(title);
-        xlList.setChanged();
-    }
+	public int print(Graphics g, PageFormat pageFormat, int page) {
+		if (page > 0)
+			return NO_SUCH_PAGE;
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+		printAll(g2d);
+		return PAGE_EXISTS;
+	}
 
-    public static void main(String[] args) {
-        new XL(new XLList(), new XLCounter());
-    }
+	public void rename(String title) {
+		setTitle(title);
+		xlList.setChanged();
+	}
+
+	public static void main(String[] args) {
+		new XL(new XLList(), new XLCounter());
+	}
 }
