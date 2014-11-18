3ffe07cb0b3128e77576acaf0d4ed049002fa309
tingiie <fatima.a.alpha@gmail.com>
2014-11-18 15:12:04 +0100

    load

--- src/gui/menu/LoadMenuItem.java
+++ src/gui/menu/LoadMenuItem.java
@@ -2,23 +2,35 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JFileChooser;
 
 import model.Sheet;
+import model.Slot;
+import model.XLBufferedReader;
 
 class LoadMenuItem extends OpenMenuItem {
- 
-    public LoadMenuItem(XL xl, StatusLabel statusLabel, Sheet shit) {
-        super(xl, statusLabel, "Load");
-    }
+	private Sheet sheet;
 
-    protected void action(String path) throws FileNotFoundException {
-        // TODO
-    	
-    }
+	public LoadMenuItem(XL xl, StatusLabel statusLabel, Sheet sheet) {
+		super(xl, statusLabel, "Load");
+		this.sheet = sheet;
+	}
 
-    protected int openDialog(JFileChooser fileChooser) {
-        return fileChooser.showOpenDialog(xl);
-    }
+	protected void action(String path) throws FileNotFoundException {
+		// TODO
+		XLBufferedReader reader = new XLBufferedReader(path);
+
+		HashMap<String, Slot> map = new HashMap();
+		sheet = new Sheet(map);
+	}
+
+	protected int openDialog(JFileChooser fileChooser) {
+		return fileChooser.showOpenDialog(xl);
+	}
 }
