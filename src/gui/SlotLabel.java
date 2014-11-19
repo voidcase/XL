88d2cbbc113d62023161bca5e69d31815ef9a692
tingiie <fatima.a.alpha@gmail.com>
2014-11-19 09:30:29 +0100

    shit

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -30,13 +30,9 @@ public class SlotLabel extends ColoredLabel implements Observer, MouseListener {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		if (arg0 instanceof Sheet){
 			sheet = (Sheet) arg0;
 			String string = sheet.getSlotText("" + col + row);
 			this.setText(string);
-		} else {
-			setBackground(Color.YELLOW);
-		}
 	}
 
 	@Override
